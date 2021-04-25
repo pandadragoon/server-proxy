652b1f64d4b69a9386e59eae66c64eabde1fc272
Joshua Edwards <josh.dean.edwards@gmail.com>
2021-04-25 12:09:05 -0500

    hosting server

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3001
+
+app.get('/api/random', async (req, res) => {
+  const apiRes = await axios.get("http://www.themealdb.com/api/json/v1/1/random.php")
+  res.send(apiRes.data);
+});
+
+app.use(express.static("public"));
+
+app.get('/api/search', async (req, res) => {
+  const apiRes = await axios.get(`http://www.themealdb.com/api/json/v1/1/search.php?s=${req.query.s}`)
+  res.send(apiRes.data)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
