e3e86a475bdc20a0ef9097d8cbeaabc557de0099
Joshua Edwards <josh.dean.edwards@gmail.com>
2021-04-25 12:16:52 -0500

    set env variable port

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 3001
+const port = process.env.PORT
 
 app.get('/api/random', async (req, res) => {
   const apiRes = await axios.get("http://www.themealdb.com/api/json/v1/1/random.php")
